3e27245f6f1d4c3b5349701dba83fa6e789ba77c
Mikal Dieatrick <mdieatrick@ewu.edu>
2023-03-13 14:00:52 -0700

    a

--- script.js
+++ script.js
@@ -141,8 +141,7 @@ const matrixGenerator = (cardValues, size = 4) => {
             //check if winCount ==half of cardValues
             if (winCount == Math.floor(cardValues.length / 2)) {
               result.innerHTML = `<h2>You Won</h2>
-            <h4>Moves: ${movesCount}</h4>
-            <h4>Time: ${timeValue}</h4>`;
+            <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
           } else {
