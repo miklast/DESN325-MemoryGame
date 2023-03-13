71ad10b6d3b2ec54f3156d1ed1e243191a19b4a4
Mikal Dieatrick <mdieatrick@ewu.edu>
2023-03-13 13:08:39 -0700

    fixed image locations

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ let secondCard = false;
 
 //Items array
 const items = [
-  { name: "bee", image: "bee.png" },
-  { name: "crocodile", image: "crocodile.png" },
-  { name: "macaw", image: "macaw.png" },
-  { name: "gorilla", image: "gorilla.png" },
-  { name: "tiger", image: "tiger.png" },
-  { name: "monkey", image: "monkey.png" },
-  { name: "chameleon", image: "chameleon.png" },
-  { name: "piranha", image: "piranha.png" },
-  { name: "anaconda", image: "anaconda.png" },
-  { name: "sloth", image: "sloth.png" },
-  { name: "cockatoo", image: "cockatoo.png" },
-  { name: "toucan", image: "toucan.png" },
+  { name: "bee", image: "img/bee.png" },
+  { name: "crocodile", image: "img/crocodile.png" },
+  { name: "macaw", image: "img/macaw.png" },
+  { name: "gorilla", image: "img/gorilla.png" },
+  { name: "tiger", image: "img/tiger.png" },
+  { name: "monkey", image: "img/monkey.png" },
+  { name: "chameleon", image: "img/chameleon.png" },
+  { name: "piranha", image: "img/piranha.png" },
+  { name: "anaconda", image: "img/anaconda.png" },
+  { name: "sloth", image: "img/sloth.png" },
+  { name: "cockatoo", image: "img/cockatoo.png" },
+  { name: "toucan", image: "img/toucan.png" },
 ];
 
 //Initial Time
